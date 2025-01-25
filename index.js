f905e6827f306c781c52393c24065c64776da22c
KingisBad <137013019+KingisBad@users.noreply.github.com>
2025-01-24 16:21:07 -0800

    add gamemodeToggle Keybind

--- index.js
+++ index.js
@@ -14,3 +14,30 @@ import "./features/AutoTimerBalance"
 register("command", () => {
     Settings().getConfig().openGui()
 }).setName("autoroutes")
+
+const gamemodeKeybind = new KeyBind("Gamemode Switcher", Keyboard.KEY_NONE, "AutoRoutes")
+
+register(net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent, () => {
+    if (Server.getIP() !== "localhost") return
+    worldLoad.register()
+    if (!gamemodeKeybind.isKeyDown() || Client.isInGui() ) return
+        toggleGameMode()
+})
+
+let gmSwitch = true
+
+function toggleGameMode() {
+    if (gmSwitch) {
+        ChatLib.command("gamemode s")
+        gmSwitch = false
+    } else {
+        ChatLib.command("gamemode c")
+        gmSwitch = true
+    }
+}
+
+const worldLoad = register("worldLoad", () => {
+    ChatLib.command("gamemode c")
+    gmSwitch = true
+    worldLoad.unregister()
+}).unregister()
